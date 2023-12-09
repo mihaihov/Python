695c96618cbf1555be974b45ca90cda1a952f7ec
mihai.hov <raducu@RO.LIDL.NET>
2023-12-09 23:15:59 +0200

    AddLinkedLists optimization

--- AddLinkedLists.py
+++ AddLinkedLists.py
@@ -31,57 +31,82 @@ def getLinkedListLength(number: ListNode) -> int:
     return len
 
 def addLinkedListNumbers(n: ListNode, m: ListNode) -> ListNode:
-    nLen = getLinkedListLength(n)
-    mLen = getLinkedListLength(m)
+    startDummy : ListNode = ListNode()
+    start = startDummy
+    
+    carry : int = 0
+    while n or m or carry:
+        nDigit = n.val if n else 0
+        mDigit = m.val if m else 0
+        
+        sum = nDigit + mDigit + carry
+        carry = sum // 10
 
-    hold : int = 0
+        start.next = ListNode(sum%10)
+        start = start.next
 
-    dummyStart = ListNode()
-    start = dummyStart
-
-    if nLen > mLen:
-        while n != None:
-            nDigit : int = n.val
-            mDigit : int  = 0
-            if m != None:
-                mDigit = m.val
-
-            digitSum = nDigit + mDigit + hold
-            hold = int(digitSum / 10)
-
-            start.next = ListNode(digitSum%10)
-            start = start.next
+        if n:
             n = n.next
-            if m != None:
-                if m.next != None:
-                    m = m.next
-                else:
-                    m = None
-        if hold != 0:
-            start.next = ListNode(hold)
-            start = start.next
-    else:942
-        while m != None:
-            mDigit : int = m.val
-            nDigit : int  = 0
-            if n != None:
-                nDigit = n.val
-
-            digitSum = nDigit + mDigit + hold
-            hold = int(digitSum / 10)
-
-            start.next = ListNode(digitSum%10)
-            start = start.next
+        if m:
             m = m.next
-            if n != None:
-                if n.next != None:
-                    n = n.next
-                else:
-                    n = None
-        if hold != 0:
-            start.next = ListNode(hold)
-            start = start.next
-    return dummyStart.next
+    
+    return startDummy.next
+
+
+
+
+
+    # nLen = getLinkedListLength(n)
+    # mLen = getLinkedListLength(m)
+
+    # hold : int = 0
+
+    # dummyStart = ListNode()
+    # start = dummyStart
+
+    # if nLen > mLen:
+    #     while n != None:
+    #         nDigit : int = n.val
+    #         mDigit : int  = 0
+    #         if m != None:
+    #             mDigit = m.val
+
+    #         digitSum = nDigit + mDigit + hold
+    #         hold = int(digitSum / 10)
+
+    #         start.next = ListNode(digitSum%10)
+    #         start = start.next
+    #         n = n.next
+    #         if m != None:
+    #             if m.next != None:
+    #                 m = m.next
+    #             else:
+    #                 m = None
+    #     if hold != 0:
+    #         start.next = ListNode(hold)
+    #         start = start.next
+    # else:942
+    #     while m != None:
+    #         mDigit : int = m.val
+    #         nDigit : int  = 0
+    #         if n != None:
+    #             nDigit = n.val
+
+    #         digitSum = nDigit + mDigit + hold
+    #         hold = int(digitSum / 10)
+
+    #         start.next = ListNode(digitSum%10)
+    #         start = start.next
+    #         m = m.next
+    #         if n != None:
+    #             if n.next != None:
+    #                 n = n.next
+    #             else:
+    #                 n = None
+    #     if hold != 0:
+    #         start.next = ListNode(hold)
+    #         start = start.next
+    # return dummyStart.next
         
 
 
